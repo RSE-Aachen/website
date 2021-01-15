d75d57aeedbadad3ecc92c40eb915e3a3f2cf6df
Lukas Lamm <lamm@ifam.rwth-aachen.de>
2021-01-15 15:39:27 +0100

    Started working on events app

--- events/views.py
+++ events/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from events.models import Event
+from datetime import date
 
 def events(request):
-    return render(request, 'events.html')
+
+    all_events = Event.objects.all()
+    upcomming_events = Event.objects.filter(date__gte=date.today())
+    past_events = Event.objects.filter(date__lt=date.today())
+
+    context = {
+        'all_events': all_events,
+        'upcomming_events': upcomming_events,
+        'past_events': past_events
+    }
+
+    return render(request, 'events.html', context=context)
